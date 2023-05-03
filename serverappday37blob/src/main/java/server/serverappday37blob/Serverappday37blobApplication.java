ce84e76a570d60ea08c029f698d17c4b64bf7fbb
aneethajude <aneetha.joseph@gmail.com>
2023-05-03 14:43:04 +0800

    First

---
+++ serverappday37blob/src/main/java/server/serverappday37blob/Serverappday37blobApplication.java
@@ -0,0 +1,13 @@
+package server.serverappday37blob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serverappday37blobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Serverappday37blobApplication.class, args);
+	}
+
+}
---
+++ serverappday37blob/src/test/java/server/serverappday37blob/Serverappday37blobApplicationTests.java
@@ -0,0 +1,13 @@
+package server.serverappday37blob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serverappday37blobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
